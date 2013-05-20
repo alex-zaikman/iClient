e111399de256d9fb089a1a2488a5c4a8d09d2cab
alex z <t2kalex@icloud.com>
2013-05-20 17:27:21 +0300

    refactoring groups

---
+++ LmsClient/controllers/LmsClientCourseViewController.h
@@ -0,0 +1,13 @@
+//
+//  LmsClientCourseViewController.h
+//  LmsClient
+//
+//  Created by alex zaikman on 5/20/13.
+//  Copyright (c) 2013 alex zaikman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LmsClientCourseViewController : UICollectionViewController
+
+@end
---
+++ LmsClient/controllers/LmsClientCourseViewController.m
@@ -0,0 +1,38 @@
+//
+//  LmsClientCourseViewController.m
+//  LmsClient
+//
+//  Created by alex zaikman on 5/20/13.
+//  Copyright (c) 2013 alex zaikman. All rights reserved.
+//
+
+#import "LmsClientCourseViewController.h"
+
+@interface LmsClientCourseViewController ()
+
+@end
+
+@implementation LmsClientCourseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
