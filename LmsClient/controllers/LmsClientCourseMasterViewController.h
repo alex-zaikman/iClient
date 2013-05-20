7bce4c0cad2cb54b9ac919919a5854a756f3027c
alex z <t2kalex@icloud.com>
2013-05-20 17:44:08 +0300

    course view controllers

---
+++ LmsClient/controllers/LmsClientCourseMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  LmsClientCourseMasterViewController.h
+//  LmsClient
+//
+//  Created by alex zaikman on 5/20/13.
+//  Copyright (c) 2013 alex zaikman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LmsClientCourseMasterViewController : UITableViewController
+
+@end
